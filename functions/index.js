6a03bb712cfbf04b65b8636d53098e9ef22491c9
juliosincable <32161739+juliosincable@users.noreply.github.com>
2020-07-04 15:56:56 -0400

    Xxxxxxxxxxxxx

--- functions/index.js
+++ functions/index.js
@@ -10,27 +10,38 @@ admin.initializeApp(functions.config().firebase);
 let db = admin.firestore();
 
 
-// aqui quiero escribir en la base de datos
-let data = {
-    name: 'Los Angeles',
-    state: 'CA',
-    country: 'USA'
-  };
-
-  let setDoc = db.collection('cities').doc('LA').set(data);
-// aqui quiero escribir en la base de datos
 
 
+ 
   //aqui quiero tratar de hacer una funcion y llamarla
 exports.addMessage = functions.https.onCall((data, context) => {
     //aqui quiero tratar de hacer una funcion y llamarla
   });
 
+  exports.myFunction = functions.firestore
+  .document('my-collection/{doc-id}')
+  .onWrite((change, context) => { /* ... */ 
+  });
 
-//aqui quiero hacer un mensaje de bienvenida 
-exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
-const uid = auth.uid;
-const name = auth.name || null;
-const email = auth.email || null;
-return sendWelcomeEmail(email);
-});
+  exports.useMultipleWildcards = functions.firestore
+  .document('users/{userId}/{messageCollectionId}/{messageId}')
+  .onCreate((change, context) => {
+    // If we set `/users/marie/incoming_messages/134` to {body: "Hello"} then
+    // context.params.userId == "marie";
+    // context.params.messageCollectionId == "incoming_messages";
+    // context.params.messageId == "134";
+    // ... and ...
+    // change.after.data() == {body: "Hello"}
+  });
+
+
+
+
+  function triggerNewDocument() {
+    exports.createUser = functions.firestore
+        .document('users/{userId}')
+        .onCreate((snap, context) => {
+          const newValue = snap.data();          
+          const name = newValue.name;
+        });
+  }
